b8e7b4d4313804606967d5d6eba4c2dd2c1ecac7
Kramer <evan.kramer@dc.gov>
2019-12-27 12:08:10 -0800

    Accessing all sheets and added `unnest_wider` to rectangularize data

--- smartsheet_api.R
+++ smartsheet_api.R
@@ -6,30 +6,34 @@ options(java.parameters = "-Xmx16G")
 library(tidyverse)
 library(lubridate)
 library(httr)
-api_key = readRegistry("Environment", hive = "HCU")$smartsheet_api_key
-api_pwd = readRegistry("Environment", hive = "HCU")$smartsheet_pwd
+library(rjson)
+ss_api_key = readRegistry("Environment", hive = "HCU")$smartsheet_api_key
+ss_api_pwd = readRegistry("Environment", hive = "HCU")$smartsheet_pwd
+qb_api_key = readRegistry("Environment", hive = "HCU")$quickbase_api_key
+qb_api_pwd = readRegistry("Environment", hive = "HCU")$quickbase_pwd
+qb_api_app = readRegistry("Environment", hive = "HCU")$quickbase_api_token
+qb_url = readRegistry("Environment", hive = "HCU")$quickbase_api_url
+api_uid = readRegistry("Environment", hive = "HCU")$email_address
 
-
-
-# Can I send a list of new data requests to Smartsheet Front Office review sheet? 
+# Send a list of new data requests to Smartsheet Front Office review sheet? 
+# Connect to Quick Base API
 con = dbConnect(
   odbc(), 
   "QuickBase via QuNect user",
   timeout = 10
 )
 
-
-
+# See which databases I have access to 
 granted_dbs = GET(
   str_c(
-    url,
+    qb_url,
     "?a", "=", "API_GrantedDBs", # API_GrantedDBs function,
-    "&", "usertoken", "=", api_key # use API user key to authenticate (could also use ticket)
+    "&", "usertoken", "=", qb_api_key # use API user key to authenticate (could also use ticket)
   )
 ) %>% 
   content()
 
-## Create a tibble and extract names and IDs from API
+# Create a tibble and extract names and IDs from API
 db = tibble(
   db_name = xml_nodes(granted_dbs, "dbname") %>% 
     str_flatten() %>% 
@@ -40,7 +44,7 @@ db = tibble(
     str_split("</dbid><dbid>") %>% 
     unlist() 
 ) %>% 
-  ## Concatenate to match QuNect naming conventions
+  # Concatenate to match QuNect naming conventions
   mutate(
     db_name = str_replace_all(db_name, "</dbname>", "") %>% 
       str_replace_all("<dbname>", "") %>% 
@@ -50,19 +54,45 @@ db = tibble(
     db_name_id = str_c(db_name, db_id, sep  = "_")
   ) 
 
+# Get all new requests from QB
+dbGetQuery(
+  con,
+  str_c(
+    "select *
+    from ", db$db_name_id[str_ends(db$db_name, "__Requests")]
+  )
+) %>% 
+  janitor::clean_names() %>% 
+  as_tibble() %>% 
+  filter(date_created >= today() - ddays(7))
+
 # Get all sheets
+# Why is it only showing 100 sheets? How can we fix that? 
 GET(
   url = "https://api.smartsheet.com/2.0/sheets",
   authenticate(
-    user = "evan.kramer@dc.gov",
-    password = api_pwd
+    user = api_uid,
+    password = ss_api_pwd
   ),
   add_headers(
-    Authorization = str_c("Bearer ", api_key)
+    Authorization = str_c("Bearer ", ss_api_key)
   )
 ) %>% 
-  content() 
+  content(as = "parsed", type = "application/json") %>% 
+  as_tibble() %>% 
+  unnest_wider(col = data) 
 
-# Get data from a sheet
-
-# DR 
+# Get information from front office sheet
+GET(
+  url = "https://api.smartsheet.com/2.0/sheets/6420298896566148",
+  authenticate(
+    user = api_uid,
+    password = ss_api_pwd
+  ),
+  add_headers(
+    Authorization = str_c("Bearer ", ss_api_key)
+  )
+) %>% 
+  content(as = "parsed", type = "application/json") %>% 
+  as_tibble() %>% 
+  unnest_wider(col = data) 
