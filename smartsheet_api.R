50b87f0c489deaa5feb7bd32922e67986e10cf28
Kramer <evan.kramer@dc.gov>
2020-02-05 17:44:34 -0500

    Removed dependency on QuNect and began pulling data directly from QB API

--- smartsheet_api.R
+++ smartsheet_api.R
@@ -7,6 +7,9 @@ library(tidyverse)
 library(lubridate)
 library(httr)
 library(rjson)
+library(odbc)
+library(rvest)
+library(xml2)
 ss_api_key = readRegistry("Environment", hive = "HCU")$smartsheet_api_key
 ss_api_pwd = readRegistry("Environment", hive = "HCU")$smartsheet_pwd
 qb_api_key = readRegistry("Environment", hive = "HCU")$quickbase_api_key
@@ -16,14 +19,7 @@ qb_url = readRegistry("Environment", hive = "HCU")$quickbase_api_url
 api_uid = readRegistry("Environment", hive = "HCU")$email_address
 
 # Send a list of new data requests to Smartsheet Front Office review sheet? 
-# Connect to Quick Base API
-con = dbConnect(
-  odbc(), 
-  "QuickBase via QuNect user",
-  timeout = 10
-)
-
-# See which databases I have access to 
+# Get DBs to which I have access
 granted_dbs = GET(
   str_c(
     qb_url,
@@ -32,8 +28,6 @@ granted_dbs = GET(
   )
 ) %>% 
   content()
-
-# Create a tibble and extract names and IDs from API
 db = tibble(
   db_name = xml_nodes(granted_dbs, "dbname") %>% 
     str_flatten() %>% 
@@ -54,36 +48,41 @@ db = tibble(
     db_name_id = str_c(db_name, db_id, sep  = "_")
   ) 
 
-# Get all new requests from QB
-dbGetQuery(
-  con,
+# Query the API for all new data requests
+new_dr = GET(
   str_c(
-    "select *
-    from ", db$db_name_id[str_ends(db$db_name, "__Requests")]
+    str_replace(qb_url, "main", db$db_id[db$db_name == "OSSE_Data_Request_Portal__Requests"]), # database ID
+    "?a", "=", "API_DoQuery", # call API_DoQuery function or API_GenResultsTable function
+    "&query={'0'.OAF.", month(today() - 7), "/", day(today() - 7), "/", year(today() - 7), "}", # query | field ID, OAF = on or after, date format
+    "&clist=a", # return all columns
+    "&", "usertoken", "=", qb_api_key # use API user key to authenticate; users need to create in Quick Base and store as environment variable
   )
 ) %>% 
-  janitor::clean_names() %>% 
-  as_tibble() %>% 
-  filter(date_created >= today() - ddays(7))
+  content()
 
-# Get all sheets
-# Why is it only showing 100 sheets? How can we fix that? 
-GET(
-  url = "https://api.smartsheet.com/2.0/sheets",
-  authenticate(
-    user = api_uid,
-    password = ss_api_pwd
-  ),
-  add_headers(
-    Authorization = str_c("Bearer ", ss_api_key)
-  )
-) %>% 
-  content(as = "parsed", type = "application/json") %>% 
-  as_tibble() %>% 
-  unnest_wider(col = data) 
+xml_find_all(new_dr, "//record") %>% 
+  xml_text() %>% 
+  trimws()
 
-# Get information from front office sheet
-GET(
+## use xml_nodes
+tibble(
+  db_name = xml_nodes(granted_dbs, "record") %>% 
+    str_flatten() %>% 
+    str_split("</dbname><dbname>") %>% 
+    unlist(),
+  db_id = xml_nodes(granted_dbs, "dbid") %>% 
+    str_flatten() %>% 
+    str_split("</dbid><dbid>") %>% 
+    unlist() 
+)
+
+
+
+
+
+
+# Find last row in Smartsheet
+dr_review = GET(
   url = "https://api.smartsheet.com/2.0/sheets/6420298896566148",
   authenticate(
     user = api_uid,
@@ -93,6 +92,11 @@ GET(
     Authorization = str_c("Bearer ", ss_api_key)
   )
 ) %>% 
-  content(as = "parsed", type = "application/json") %>% 
-  as_tibble() %>% 
-  unnest_wider(col = data) 
+  content(as = "parsed", type = "application/json") 
+dr_review$totalRowCount
+
+
+
+
+
+
