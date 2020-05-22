f8f5be27fbfcb4a4d28e31f9b30f46af8c131114
Kramer <evan.kramer@dc.gov>
2020-05-22 17:42:19 -0400

    Cleaned up repo and created script to source all specified files

---
+++ source_all.R
@@ -0,0 +1,36 @@
+# Source All QB/Smartsheet API Scripts
+# Evan Kramer
+
+# Attach packages
+options(java.parameters = "-Xmx16G")
+library(tidyverse)
+
+# Create function
+source_all = function(dir, files, ...) {
+  # Check if directory exists
+  if(dir.exists(dir)) {
+    # Run for each script
+    for(f in files) {
+      # Check that files is not empty
+      if(length(files) > 0) {
+        # Check that all files exist in specified directory
+        if(f %in% list.files(dir)) {    
+          # Run the script and print to console
+          source(f, ...)  
+        } else {
+          message(str_c('"', f, '" not found in "', dir, '"'))
+        }
+      } else {
+        message("No files specified")
+      }
+    }
+  } else {
+    message(str_c('Directory "', dir, '" not found'))
+  }
+}
+
+# Call function
+dir = "X:/Analysis Team/Development"
+files = c()
+
+source_all(dir, files)
